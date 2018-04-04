cd8e00514e733226c9504c4e42c8b3aa23bb69e1
Jeff Lai <winfield301@gmail.com>
2018-04-04 16:33:27 +0800

    Include email in oauth

--- src/app.js
+++ src/app.js
@@ -8,15 +8,18 @@ import { Strategy } from 'passport-twitter'
 const app = new Koa()
 const router = new koaRouter()
 const { PORT = 3000 } = process.env
-console.log(process.env.TWITTER_COMSUMER_KEY)
+
 passport.use(
   new Strategy(
     {
-      consumerKey: process.env.TWITTER_COMSUMER_KEY,
-      consumerSecret: process.env.TWITTER_COMSUMER_SECRET,
+      consumerKey: process.env.TWITTER_CONSUMER_KEY,
+      consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+      userProfileURL:
+        'https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true',
       callbackURL: 'http://127.0.0.1:3000/auth/twitter/callback',
     },
     function(token, tokenSecret, profile, cb) {
+      console.log(profile.emails)
       User.findOrCreate({ twitterId: profile.id }, function(err, user) {
         return cb(err, user)
       })
