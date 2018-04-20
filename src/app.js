ada406ba120bafa4b7ac06cd5a2f54029c46b59a
Jeff Lai <winfield301@gmail.com>
2018-04-20 15:38:37 +0800

    Separate auth logic out of app.js to auth.js

--- src/app.js
+++ src/app.js
@@ -1,71 +1,20 @@
 import Koa from 'koa'
-import koaRouter from 'koa-router'
 import session from 'koa-session'
 import bodyParser from 'koa-bodyparser'
 import passport from 'koa-passport'
-import { Strategy } from 'passport-twitter'
 
-import { findUserByUuid, findOrCreateByTwitter } from './db'
+import auth from './auth'
 
 const app = new Koa()
-const router = new koaRouter()
 const { PORT = 3000 } = process.env
-
-passport.use(
-  new Strategy(
-    {
-      consumerKey: process.env.TWITTER_CONSUMER_KEY,
-      consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-      userProfileURL:
-        'https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true',
-      callbackURL: 'http://127.0.0.1:3000/auth/twitter/callback',
-    },
-    async function(token, tokenSecret, profile, cb) {
-      const profileJson = profile._json
-      const params = {
-        name: profileJson.screen_name,
-        twitterId: profileJson.id,
-        email: profileJson.email,
-      }
-      try {
-        const { rows } = await findOrCreateByTwitter(params)
-        return cb(null, rows[0])
-      } catch (e) {
-        return cb(e)
-      }
-    },
-  ),
-)
-
-passport.serializeUser((user, done) => {
-  done(null, user.uuid)
-})
-
-passport.deserializeUser(async (uuid, done) => {
-  const { rows } = await findUserByUuid(uuid)
-  done(null, rows[0])
-})
-
-router.get('/auth/twitter', passport.authenticate('twitter'))
-
-router.get('/auth/twitter/callback', (ctx, next) => {
-  return passport.authenticate(
-    'twitter',
-    { failureRedirect: '/login' },
-    (err, user, info) => {
-      ctx.login(user)
-      ctx.redirect('/')
-    },
-  )(ctx)
-})
 
 app.use(bodyParser())
 app.keys = ['secret']
 app.use(session({}, app))
 app.use(passport.initialize())
 app.use(passport.session())
-app.use(router.routes())
-app.use(router.allowedMethods())
+app.use(auth.routes())
+app.use(auth.allowedMethods())
 
 app.use(ctx => {
   const body = ctx.isAuthenticated() ? 'hello koa' : '401'
---
+++ src/auth.js
@@ -0,0 +1,57 @@
+import koaRouter from 'koa-router'
+import passport from 'koa-passport'
+import { Strategy } from 'passport-twitter'
+
+import { findUserByUuid, findOrCreateByTwitter } from './db'
+
+const router = new koaRouter()
+
+passport.use(
+  new Strategy(
+    {
+      consumerKey: process.env.TWITTER_CONSUMER_KEY,
+      consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+      userProfileURL:
+        'https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true',
+      callbackURL: 'http://127.0.0.1:3000/auth/twitter/callback',
+    },
+    async function(token, tokenSecret, profile, cb) {
+      const profileJson = profile._json
+      const params = {
+        name: profileJson.screen_name,
+        twitterId: profileJson.id,
+        email: profileJson.email,
+      }
+      try {
+        const { rows } = await findOrCreateByTwitter(params)
+        return cb(null, rows[0])
+      } catch (e) {
+        return cb(e)
+      }
+    },
+  ),
+)
+
+passport.serializeUser((user, done) => {
+  done(null, user.uuid)
+})
+
+passport.deserializeUser(async (uuid, done) => {
+  const { rows } = await findUserByUuid(uuid)
+  done(null, rows[0])
+})
+
+router.get('/auth/twitter', passport.authenticate('twitter'))
+
+router.get('/auth/twitter/callback', (ctx, next) => {
+  return passport.authenticate(
+    'twitter',
+    { failureRedirect: '/login' },
+    (err, user, info) => {
+      ctx.login(user)
+      ctx.redirect('/')
+    },
+  )(ctx)
+})
+
+export default router
