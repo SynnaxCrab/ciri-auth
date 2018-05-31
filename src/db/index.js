da10d6900794c46da177fbe0e2768428e9d422af
Jeff Lai <winfield301@gmail.com>
2018-05-31 22:50:49 +0800

    Refactor db module

--- src/db/index.js
+++ src/db/index.js
@@ -7,20 +7,75 @@ import uuidv4 from 'uuid/v4'
 const pool = new Pool()
 const debug = Debug('db')
 
-const identityExistsSQL = uid => sql`
+const QUERY = async (sql, client = pool) => {
+  try {
+    const { rows } = await client.query(sql)
+  } catch (e) {
+    debug(e)
+    throw e
+  }
+
+  return rows
+}
+/**
+ * Identity exists check SQL
+ *
+ * @param {String} uid
+ * @param {String} provider
+ * @returns {String}
+ *
+ */
+
+const identityExistsSQL = (uid, provider) => sql`
   SELECT user_id
   FROM identities
-  ${WHERE({ uid })}
+  ${WHERE({ uid, provider })}
 `
 
-const createIdentitySQL = ({ uid, userId }) => sql`
+/**
+ * Check whether identity exists
+ *
+ * @param {String} uid
+ * @param {String} provider
+ * @returns {String}
+ *
+ */
+
+const identityExists = async (uid, provider) => {
+  const rows = await QUERY(identityExistsSQL(uid, provider))
+  return rows[0]
+}
+
+/**
+ * Create Identity SQL
+ *
+ * @param {Object} { uid, userId, provider }
+ * @returns {Object}
+ */
+
+const createIdentitySQL = ({ uid, userId, provider }) => sql`
   ${INSERT('identities', {
     uuid: uuidv4(),
     uid,
-    provider: 'twitter',
+    provider,
     user_id: userId,
   })}
 `
+/**
+ * Create Identity
+ *
+ * @param {any} data
+ */
+
+export const createIdentity = async (data, client = pool) =>
+  await QUERY(createIdentitySQL(data), client)
+
+/**
+ * Create User SQL
+ *
+ * @param {Object} { email, name }
+ * @returns {String}
+ */
 
 const createUserSQL = ({ email, name }) => sql`
   ${INSERT('users', {
@@ -30,6 +85,24 @@ const createUserSQL = ({ email, name }) => sql`
   })}
   RETURNING id
 `
+/**
+ * Create User
+ *
+ * @param {Object} data
+ * @returns {Object}
+ */
+
+export const createUser = async (data, client = pool) => {
+  const { rows } = await QUERY(createUserSQL(data, client))
+  return rows[0]
+}
+
+/**
+ * User SQL by ID
+ *
+ * @param {String} id
+ * @returns {String}
+ */
 
 const userSQL = id => sql`
   SELECT *
@@ -37,19 +110,65 @@ const userSQL = id => sql`
   ${WHERE({ id })}
 `
 
+/**
+ * Find User with ID
+ *
+ * @param {String} id
+ * @returns {Object}
+ */
+
+export const findUser = async id => {
+  const { rows } = await QUERY(userSQL(id))
+  return rows[0]
+}
+
+/**
+ * User SQL by uuid
+ *
+ * @param {any} uuid
+ */
+
 const userSQLByUuid = uuid => sql`
   SELECT *
   FROM users
   ${WHERE({ uuid })}
 `
 
-export const findUserByUuid = async uuid => {
+/**
+ * Find User by uuid
+ *
+ * @param {any} uuid
+ * @returns {Object}
+ */
+
+const findUserByUuid = async uuid => {
+  const { rows } = await QUERY(userSQLByUuid(uuid))
+  return rows[0]
+}
+
+export const findOrCreateUserByGithub = async ({ node_id, email, name }) => {
+  const client = await pool.connect()
   try {
-    const { rows } = await pool.query(userSQLByUuid(uuid))
-    return rows[0]
+    const userId = await identityExists(node_id, 'github')
+
+    if (!userId) {
+      await client.query('BEGIN')
+      const { id: createdUserId } = await createUser({ email, name }, client)
+      await createIdentity(
+        {
+          uid: node_id,
+          userId: createdUserId,
+        },
+        client,
+      )
+      await client.query('COMMIT')
+    }
   } catch (e) {
+    await client.query('ROLLBACK')
     debug(e)
     throw e
+  } finally {
+    client.release(true)
   }
 }
 
