aad92c0c11fcddc681eb1f71213447de721de563
Jeff Lai <winfield301@gmail.com>
2018-06-05 11:58:11 +0800

    Set access token only when it's invalid

--- src/access_token.js
+++ src/access_token.js
@@ -1,18 +1,24 @@
 import jwt from 'jsonwebtoken'
 
-export const accessToken = user =>
+export const generateAccessToken = user =>
+  // TODO: set expiresIn in env
   jwt.sign({ id: user.uuid }, process.env.SECRET, { expiresIn: '1d' })
 
 export default () => {
   return async function(ctx, next) {
     if (ctx.user) {
-      ctx.cookies.set('access_token', accessToken(ctx.user), {
-        domain: process.domain,
-        signed: true,
-        // TODO: set them to true in production & staging
-        // secure: true,
-        // httpOnly: true,
-      })
+      const accessToken = ctx.cookies.get('access_token', { signed: true })
+      try {
+        jwt.verify(accessToken, process.env.SECRET)
+      } catch (e) {
+        ctx.cookies.set('access_token', generateAccessToken(ctx.user), {
+          domain: process.domain,
+          signed: true,
+          // TODO: set them to true in production & staging
+          // secure: true,
+          // httpOnly: true,
+        })
+      }
     }
 
     await next()
