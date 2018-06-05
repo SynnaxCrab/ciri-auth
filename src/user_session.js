763cc6fe8f607dbfdbb503834997bbc900a47095
Jeff Lai <winfield301@gmail.com>
2018-06-05 11:32:08 +0800

    Fix user session

--- src/user_session.js
+++ src/user_session.js
@@ -2,8 +2,10 @@ import { findUserByUuid } from './db'
 
 export default () => {
   return async (ctx, next) => {
-    const user = await findUserByUuid(ctx.session.user)
-    ctx.user = user
+    if (ctx.session.user) {
+      const user = await findUserByUuid(ctx.session.user)
+      ctx.user = user
+    }
     await next()
   }
 }
